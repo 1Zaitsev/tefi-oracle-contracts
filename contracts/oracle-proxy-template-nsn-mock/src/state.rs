47599f99b3362b5547973ebb8719d5bd637f08b7
1Zaitsev <4yu@bk.ru>
2022-04-11 12:53:36 +0300

    state in progress

--- contracts/oracle-proxy-template-nsn-mock/src/state.rs
+++ contracts/oracle-proxy-template-nsn-mock/src/state.rs
@@ -1,16 +1,36 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Decimal, StdResult, Storage};
+use cw_storage_plus::{Item, Map};
+use tefi_oracle::proxy::ProxyPriceResponse;
 
 use crate::msg::ConfigResponse;
 
 pub const CONFIG: Item<Config> = Item::new("config");
+// key - symbol: &str
+// value - ProxyPriceResponse
+pub const KEY_PRICES: Map<&str, ProxyPriceResponse> = Map::new("prices");
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 pub struct Config {
     pub source_addr: Addr,
+}
+
+pub fn upsert_price(storage: &mut dyn Storage, symbol: &str, rate: Decimal) -> StdResult<ProxyPriceResponse> {
+    match KEY_PRICES.may_load(storage, symbol)?{
+        None => {
+            //TODO: calculate_curent_time and safe as last_updated
+            let last_updated = 0;
+            let response = ProxyPriceResponse{
+                rate,
+                last_updated
+            };
+            KEY_PRICES.save(storage, symbol, &response)?;
+            Ok(response)
+        },
+        Some(value) => {//TODO: update},
+    }
 }
 
 impl Config {
